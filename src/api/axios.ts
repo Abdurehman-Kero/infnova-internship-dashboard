b76c3cefa1dd973e85c2911ae8219c275127161f
abdurehman-kero <keroabdurehman@gmail.com>
2026-07-22 17:32:45 -0400

    feat: configure axios client and API layer

---
+++ src/api/axios.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { API_BASE_URL } from "../constants/api";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("accessToken");
+    }
+
+    return Promise.reject(error);
+  },
+);
+export default api;
